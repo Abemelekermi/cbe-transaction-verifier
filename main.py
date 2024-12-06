28689f9e2c0c12b7492e2a68cff6049095ee8fbc
Abemelekermi <103841321+Abemelekermi@users.noreply.github.com>
2024-12-06 19:37:50 +0300

    updated main.py

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import requests
 import time
+
 def verifyTransaction(transaction_id, account_number, download_pdf=False):
     url = f"https://apps.cbe.com.et:100/?id={transaction_id}{account_number}"
-    response = requests.get(url, verify=False)
+    try:
+        response = requests.get(url, verify=False)
+        response.raise_for_status()
 
-    if response.status_code == 200:
-        if(download_pdf):
-            with open(f"{str(time.time())}.pdf", 'wb') as f:
+        if download_pdf:
+            filename = f"{int(time.time()*int(account_number))}.pdf"
+            with open(filename, 'wb') as f:
                 f.write(response.content)
-            print("PDF downloaded successfully")
-        return response.status_code
-    else:
+            print(f"PDF downloaded successfully")
+
         return response.status_code
 
-transaction_id = "your_transaction_id"
-account_number = "your_or_receiver_account_number"
-verifyTransaction(transaction_id, account_number,True)
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+transaction_id = "FT24110G8YL1"
+account_number = "42690937"
+status_code = verifyTransaction(transaction_id, account_number, True)
+if status_code is not None:
+    print(f"Status code: {status_code}")
